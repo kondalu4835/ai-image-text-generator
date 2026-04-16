1672c49c7eededb45abe029d28a3975e53958faf
kondalu4835 <yedukondalumogili816@gmail.com>
2026-04-16 09:09:42 +0530

    Updated API URL for deployment

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:5000";
+const API = "https://ai-backend-6lru.onrender.com";
 
 async function enhanceText() {
   document.getElementById("loadingText").style.display = "block";
